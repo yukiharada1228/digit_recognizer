9e46c801dad342fa02a5b57026b53bce7680e0d8
yukiharada1228 <yukiharada1228@gmail.com>
2022-12-08 09:33:28 +0900

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -7,12 +7,13 @@ from torch.utils.data import Dataset
 
 logger = logging.getLogger(__name__)
 
+
 class MNISTDataset(Dataset):
-    
+
     def __init__(self, dataframe, transform):
-        
+
         self.df = dataframe
-        
+
         channel = 1
         width = 28
         height = 28
@@ -22,24 +23,27 @@ class MNISTDataset(Dataset):
             self.X, self.y = self.test()
         else:
             self.X, self.y = self.train()
-        
+
         self.tf = transform
-    
+
     def train(self):
-        X = self.df.drop('label', axis=1).values.reshape(-1, 28, 28).astype(np.uint8)[..., np.newaxis]
+        X = self.df.drop('label', axis=1).values.reshape(-1, 28, 28)
+        X = X.astype(np.uint8)[..., np.newaxis]
         y = torch.from_numpy(self.df['label'].values)
-        logger.debug({'action': 'train', 'X.shape': X.shape, 'y.size()': y.size()})
+        logger.debug(
+            {'action': 'train', 'X.shape': X.shape, 'y.size()': y.size()})
         return X, y
-    
+
     def test(self):
-        X = self.df.values.reshape(-1, 28, 28).astype(np.uint8)[..., np.newaxis]
+        X = self.df.values.reshape(-1, 28, 28)
+        X = X.astype(np.uint8)[..., np.newaxis]
         y = None
         logger.debug({'action': 'test', 'X.shape': X.shape, 'y': y})
         return X, y
-    
+
     def __len__(self):
         return len(self.X)
-    
+
     def __getitem__(self, idx):
         if self.y is None:
             # test
@@ -49,8 +53,8 @@ class MNISTDataset(Dataset):
         else:
             # train
             X, y = self.tf(self.X[idx]), self.y[idx]
-            logger.debug({'action': '__getitem__', 
-                          'X.size()': X.size(),  
+            logger.debug({'action': '__getitem__',
+                          'X.size()': X.size(),
                           'y.size()': y.size()})
             return X, y
 
@@ -63,16 +67,15 @@ if __name__ == '__main__':
     from torch.utils.data import DataLoader
     from torchvision import transforms
 
-
-    logging.basicConfig(level=logging.DEBUG, 
+    logging.basicConfig(level=logging.DEBUG,
                         stream=sys.stdout)
 
     train_df = pd.read_csv('./train.csv')
     test_df = pd.read_csv('./test.csv')
 
-    transform = transforms.Compose([transforms.ToPILImage(), 
+    transform = transforms.Compose([transforms.ToPILImage(),
                                     transforms.ToTensor()])
-    
+
     train = MNISTDataset(train_df, transform=transform)
     test = MNISTDataset(test_df, transform=transform)
 
